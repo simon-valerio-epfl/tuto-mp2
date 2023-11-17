97310d2fff2ea1803bb75f2afa1afd2054855382
Androz2091 <androz2091@gmail.com>
2023-11-17 15:07:25 +0100

    next

--- tutos/src/main/java/ch/epfl/cs107/play/tuto1/Tuto1.java
+++ tutos/src/main/java/ch/epfl/cs107/play/tuto1/Tuto1.java
@@ -3,6 +3,8 @@ package ch.epfl.cs107.play.tuto1;
 import ch.epfl.cs107.play.areagame.AreaGame;
 import ch.epfl.cs107.play.engine.actor.TextGraphics;
 import ch.epfl.cs107.play.io.FileSystem;
+import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.tuto1.actor.SimpleGhost;
 import ch.epfl.cs107.play.tuto1.area.maps.Ferme;
 import ch.epfl.cs107.play.tuto1.area.maps.Village;
 import ch.epfl.cs107.play.window.Window;
@@ -10,6 +12,8 @@ import ch.epfl.cs107.play.window.Window;
 import java.awt.*;
 
 public class Tuto1 extends AreaGame {
+
+    private SimpleGhost player;
 
     private void createAreas() {
         addArea(new Ferme());
@@ -19,7 +23,13 @@ public class Tuto1 extends AreaGame {
     public boolean begin(Window window, FileSystem fileSystem) {
         if (super.begin(window, fileSystem)) {
             createAreas();
-            this.setCurrentArea("zelda/Village", true);
+            this.setCurrentArea("zelda/Ferme", true);
+
+            player = new SimpleGhost(new Vector(18, 7), "ghost.1");
+            this.getCurrentArea().registerActor(player);
+
+            this.getCurrentArea().setViewCandidate(player);
+
             return true;
         } else return false;
     }
